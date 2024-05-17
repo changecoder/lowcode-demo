4e8d264dd4a6ccba493b59fbbad92e4fce06cb43
changecoder <changecoder@outlook.com>
2024-05-17 08:17:20 +0800

    更新示例

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,40 @@
 import './style.css'
 
-const { init } = (window as any).CCLowcodeEngine
+const { init, material } = (window as any).CCLowcodeEngine
 
 const container = document.querySelector('#app')
 
 init(container)
+
+material.loadIncrementalAssets({
+  "version": "0.1.0",
+  "packages": [
+    {
+      "package": "lowcode-material-ant-vue",
+      "version": '0.1.1',
+      "library": "LowcodeMaterialAntVue",
+      "urls": [
+        "https://cdn.jsdelivr.net/npm/lowcode-material-ant-vue@0.1.1/dist/index.js",
+        "https://cdn.jsdelivr.net/npm/lowcode-material-ant-vue@0.1.1/dist/index.css"
+      ]
+    }
+  ],
+  "components": [
+    {
+      "exportName": "LowcodeMaterialAntVueMeta",
+      "npm": {
+        "package": "lowcode-material-ant-vue",
+        "version": "0.1.1"
+      },
+      "url": "https://cdn.jsdelivr.net/npm/lowcode-material-ant-vue@0.1.1/dist/meta.js",
+      "urls": {
+        "default": "https://cdn.jsdelivr.net/npm/lowcode-material-ant-vue@0.1.1/dist/meta.js"
+      }
+    }
+  ],
+  "sort": {
+    "groupList": ["精选组件", "原子组件"],
+    "categoryList": ["通用", "导航", "信息输入", "信息展示", "信息反馈"]
+  },
+  "groupList": ["精选组件", "原子组件"]
+})
